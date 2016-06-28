a2f1b4be175addb81c6317e07de21da41bb63d00
Milinda Pathirage <milinda.pathirage@gmail.com>
2016-06-28 15:26:21 -0400

    removed some unused imports

--- fdbench-core/src/test/java/org/pathirage/fdbench/metrics/MetricsTest.java
+++ fdbench-core/src/test/java/org/pathirage/fdbench/metrics/MetricsTest.java
@@ -25,13 +25,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Collections;
 
 public class MetricsTest {
   private static final Logger log = LoggerFactory.getLogger(MetricsTest.class);
